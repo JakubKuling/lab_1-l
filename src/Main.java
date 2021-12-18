57c8c22c1a4eba29037d206c1221242e182aa74f
kubak <kuba8983@wp.pl>
2021-12-18 14:36:36 +0100

    zadanko

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
+        System.out.println("Hello world");
+        System.out.println("jeden");
+        System.out.println("dwa");
+
+        System.out.print("ka");
+        System.out.print("wa");
+        System.out.print("łek");
 
     }
 }
